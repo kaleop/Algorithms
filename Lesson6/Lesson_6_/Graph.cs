753584c715d7860bedc669a248c1e0667eac842a
Victor <kaleop@mail.ru>
2021-07-26 13:32:41 +0300

    Minimum length in graph

---
+++ Lesson6/Lesson_6_/Graph.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6_
+{
+
+    public class Graph
+    {
+        public string Name { get; set; }
+        public List<(Graph destination,int weight)> Destinations { get; private set; }
+
+        public Graph(string name)
+        {
+            Name = name;
+            Destinations = new List<(Graph destination, int weight)>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Graph a = obj as Graph;
+            if (a == null) return false;
+            return a.Name == Name ? true : false;
+        }
+
+        public void AddDestination(Graph nameDestination, int weight)
+        {
+            Destinations.Add((nameDestination, weight));
+            nameDestination.Destinations.Add((this,weight));
+        }
+
+        public void AddDestination(Graph startNode, Graph nameDestination, int weight)
+        {
+            startNode.AddDestination(nameDestination, weight);
+        }
+    }
+
+    public enum CommentFlag
+    {
+        Yes, No
+    }
+
+    public static class GraphHelper
+    {
+        static private Queue<Graph> workList = new Queue<Graph>();
+        static private List<Graph> blackList = new List<Graph>();
+        static private Graph node = null;
+
+        // Заполняет очередь не повторяющимися элементами
+        private static void AddInWorkList( Queue<Graph> workList, Graph currentGraph, List<Graph> blackList, CommentFlag cf)
+        {
+            if (cf == CommentFlag.Yes)
+            {
+                foreach (var item in currentGraph.Destinations)
+                {
+                    if (!workList.Contains(item.destination) && !blackList.Contains(item.destination))
+                    {
+                        Console.WriteLine($"{item.destination.Name} добавлен в очередь");
+                        workList.Enqueue(item.destination);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var item in currentGraph.Destinations)
+                {
+                    if (!workList.Contains(item.destination) && !blackList.Contains(item.destination))
+                    {
+                        workList.Enqueue(item.destination);
+                    }
+                }
+            }
+        }
+
+        public static Graph FindNode(Graph startGraphNode, string name)
+        {
+            AllClear();
+            
+            workList.Enqueue(startGraphNode);
+
+            while (workList.Count > 0)
+            {
+                node = workList.Dequeue();
+                if (node.Name == name) return node;
+
+                if (!blackList.Contains(node))
+                {
+                    blackList.Add(node);
+                    AddInWorkList(workList, node, blackList, CommentFlag.No);
+                }
+
+            }
+            return node;
+        }
+
+        /// <summary>
+        /// Возвращает коллекцию с минимальными маршрутами до каждой вершины из startNode
+        /// </summary>
+        /// <param name="startNode"> начало пути</param>
+        /// <returns></returns>
+        public static Dictionary<Graph, int> FindAllMinWeight(Graph startNode, CommentFlag cf)
+        {
+            AllClear();
+            int count = GetCount(startNode);
+            Graph[] allNode = new Graph[count];
+            int[] allWeight = new int[count];
+
+            Dictionary<Graph, int> minWeightForAll = new Dictionary<Graph, int>();
+
+            workList.Enqueue(startNode);
+            minWeightForAll.Add(startNode, 0);
+
+            while (workList.Count > 0)
+            {
+                node = workList.Dequeue();
+
+                foreach (var item in node.Destinations)
+                {
+                    // Добавляем элемент в колекцию, если его нет. Длина маршрута = вес + вес предка
+                    if (!minWeightForAll.ContainsKey(item.destination))
+                    {
+                        minWeightForAll.Add(item.destination, minWeightForAll[node]+item.weight);
+                        workList.Enqueue(item.destination);
+                    }
+                    else
+                    {
+                        // Меняем вес пути к элементу если значение меньше чем в коллекции
+                        if (minWeightForAll[node] + item.weight < minWeightForAll[item.destination])
+                        {
+                            minWeightForAll[item.destination] = minWeightForAll[node] + item.weight;
+                            workList.Enqueue(item.destination);
+                        }
+                    }
+
+                    
+
+                }
+
+            }
+
+            if (cf == CommentFlag.Yes)
+            {
+                foreach (var item in minWeightForAll)
+                {
+                    Console.WriteLine($" Из {startNode.Name} можно попасть в {item.Key.Name} минимум за {item.Value}");
+                }
+            }
+
+            return minWeightForAll;
+        }
+
+
+        // Использование Dictionary было не целесообразно
+        public static Dictionary<Graph, int> FindMinPath(Graph startNode, Graph finishNode)
+        {
+            AllClear();
+            Dictionary<Graph, int> minPath = new Dictionary<Graph, int>();
+            Dictionary<Graph, int> allMinWeight = FindAllMinWeight(startNode, CommentFlag.Yes);
+            
+            Graph node = finishNode;
+
+            minPath.Add(finishNode, allMinWeight[finishNode]);
+
+
+            while (node != startNode)
+            {
+                foreach (var item in node.Destinations)
+                {
+                    if ((allMinWeight[node] - item.weight)== allMinWeight[item.destination])
+                    {
+                        minPath.Add(item.destination, allMinWeight[item.destination]);
+                        node = item.destination;
+                        break;
+                    }
+                }
+            }
+
+            return minPath;
+        }
+
+        private static void AllClear()
+        {
+            workList.Clear();
+            blackList.Clear();
+            node = null;
+        }
+
+        public static int GetCount(Graph startGraphNode)
+        {
+            AllClear();
+            workList.Enqueue(startGraphNode);
+
+            while (workList.Count > 0)
+            {
+                node = workList.Dequeue();
+
+                if (!blackList.Contains(node))
+                {
+                    blackList.Add(node);
+                    AddInWorkList(workList, node, blackList, CommentFlag.No);
+                }
+
+            }
+
+            return blackList.Count;
+        }
+
+        public static void Bypass (Graph startGraphNode)
+        {
+            AllClear();
+
+            Console.WriteLine($"{startGraphNode.Name} добавлен в очередь");
+            workList.Enqueue(startGraphNode);
+
+            while (workList.Count>0)
+            {
+                node = workList.Dequeue();
+                Console.WriteLine($"{node.Name} удалён из очереди");
+
+                if (!blackList.Contains(node))
+                {
+                    Console.WriteLine($"{node.Name} добавлен в чёрный список");
+                    blackList.Add(node);
+                    AddInWorkList(workList, node, blackList, CommentFlag.Yes);
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Lesson6/Lesson_6_/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_6_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph v1 = new Graph("V1");
+            Graph v2 = new Graph("V2");
+            Graph v3 = new Graph("V3");
+            Graph v4 = new Graph("V4");
+            Graph v5 = new Graph("V5");
+            Graph v6 = new Graph("V6");
+            Graph v7 = new Graph("V7");
+            Graph v8 = new Graph("V8");
+
+            v1.AddDestination(v2, 32);
+            v1.AddDestination(v3, 95);
+            v1.AddDestination(v4, 75);
+            v1.AddDestination(v5, 57);
+
+            v2.AddDestination(v3, 5);
+            v2.AddDestination(v5, 23);
+            v2.AddDestination(v8, 16);
+
+            v3.AddDestination(v4, 18);
+            v3.AddDestination(v6, 6);
+
+            v4.AddDestination(v5, 24);
+            v4.AddDestination(v6, 9);
+
+            v5.AddDestination(v6, 11);
+            v5.AddDestination(v7, 20);
+            v5.AddDestination(v8, 94);
+
+            v6.AddDestination(v7, 7);
+
+            v7.AddDestination(v8, 81);
+
+            Graph start;
+            Graph finish;
+
+
+            start = v1;
+            finish = v7;
+            Dictionary<Graph, int> b = GraphHelper.FindMinPath(start, finish);
+            
+            Console.WriteLine();
+
+            Console.WriteLine($"Начало маршрута {start.Name}");
+            foreach (var item in b)
+            {
+                Console.WriteLine($"{item.Key.Name} _ {item.Value}");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
